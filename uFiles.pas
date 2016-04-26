3e4b9e63d067d03e2f5efbb614fa6d5a599d0ed1
efsfilho <efdsfilho@gmail.com>
2016-04-26 10:03:52 -0300

    Added uFiles

---
+++ uFiles.pas
@@ -0,0 +1,26 @@
+unit uFiles;
+
+interface
+uses SysUtils;
+  procedure WriteToFile(tx, dir: string);
+
+implementation
+
+procedure WriteToFile(tx, dir: string);
+var
+  F:textfile;
+begin
+  Assignfile(F,dir);
+  if not FileExists(dir) Then
+    begin
+      Rewrite(f);
+    end
+  else
+  begin
+    Append(F);
+  end;
+  Writeln(F,tx);
+  Closefile(F);
+end;
+
+end.
